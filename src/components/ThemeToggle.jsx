d092c20aa95e82a27bffb258bc5e144eb9cdeb74
Prudhomme Pierre <pierre.prudhomme@limpide.net>
2025-07-08 11:57:36 +0200

    Theme toggle fix

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -30,9 +30,10 @@ export const ThemeToggle = () => {
 
   return (
     <button onClick={toggleTheme} className={cn(
-      "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-      "focus:outlin-hidden"
-
+      "fixed z-50 p-2 rounded-full transition-colors duration-300",
+      "top-14 right-5 max-sm:top-auto max-sm:bottom-6 max-sm:right-6",
+      "bg-white/10 dark:bg-gray-800/10 backdrop-blur-md shadow-md",
+      "focus:outline-hidden"
     )}>
 
       {isDarkMode ? ( <Sun className="h-6 w-6 text-yellow-300" /> )
